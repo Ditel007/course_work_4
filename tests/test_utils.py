ea9d71d1fe5c6966f4a8bdf5530fe51508953d5e
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-18 01:46:44 +0500

    добавлены доптесты для увеличения покрытия

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 import pytest as pytest
 import json
 
-from src.utils import load_json, get_recent_payments, hide, create_payment
+from src import utils
+from src.utils import load_json, get_recent_payments, hide, create_payment, latest_correct_payments, show_payment
+from src.main import main
 
 
 def test_load_file_positive():
@@ -36,4 +38,59 @@ def test_hide():
     assert hide("1234567890123456") == "1234 56** **** 3456"
     assert hide("75106830613657916952") == "**6952"
 
+
+def test_latest_correct_payments():
+    filename = "/Users/macditel/PycharmProjects/course_work_4/tests/src/test_recent_payments.json"
+    assert latest_correct_payments(filename, 3)
+
+
+def test_create_payment_negative():
+    # Arrange
+    pay_information = {
+        "id": 123,
+        "state": "completed",
+        "date": "2022-01-01",
+        "operationAmount": 100.0,
+        "description": "Test payment",
+        "from": "John Doe",
+        "to": "Jane Doe"
+    }
+    try:
+        create_payment(pay_information)
+    except AttributeError:
+        print("Не верно заданы данные")
+
+
+def test_show_payment_negative():
+    pay_information = {
+        "id": 123,
+        "state": "completed",
+        "date": "2022-01-01",
+        "operationAmount": 100.0,
+        "description": "Test payment",
+        "from": "John Doe",
+        "to": "Jane Doe"
+    }
+    try:
+        show_payment(pay_information)
+    except AttributeError:
+        print("Не верно заданы данные")
+
+
+def test_recent_payments_non_exist():
+    filename = "/Users/macditel/PycharmProjects/course_work_4/tests/src/testy_recent_payments.json"
+    parameters = {"id", "date", "state", "operationAmount", "description", "to"}
+    try:
+        get_recent_payments(filename, 4, parameters)
+    except FileNotFoundError:
+        print("Неверно переданы данные в функцию")
+
+
+def test_recent_payments_non_list():
+    filename = "/Users/macditel/PycharmProjects/course_work_4/tests/src/non_list.json"
+    parameters = {"id", "date", "state", "operationAmount", "description", "to"}
+    try:
+        get_recent_payments(filename, 4, parameters)
+    except TypeError:
+        print("Неверно переданы данные в функцию")
 
